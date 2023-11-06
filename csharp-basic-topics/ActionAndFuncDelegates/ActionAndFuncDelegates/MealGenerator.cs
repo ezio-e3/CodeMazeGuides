d035838679e4476b7ecd3b9068dd8605fa5cd06b
Edinam Ahadzie <edin22.ey@gmail.com>
2023-11-06 14:46:38 +0000

    fixed spacing issues

--- csharp-basic-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/MealGenerator.cs
+++ csharp-basic-topics/ActionAndFuncDelegates/ActionAndFuncDelegates/MealGenerator.cs
@@ -4,16 +4,18 @@ public class MealGenerator
 {
     private Func<List<string>, string> generateMeal;
     private Action<string> displayMeal;
+
     public MealGenerator(Func<List<string>, string> mealGenerator, Action<string> mealDisplay)
     {
         generateMeal = mealGenerator;
         displayMeal = mealDisplay;
     }
-    
+
     public string GenerateAndDisplayMeal(List<string> ingredients)
     {
         var meal = generateMeal(ingredients);
         displayMeal(meal);
+        
         return "order completed succesfully";
     }
 }
